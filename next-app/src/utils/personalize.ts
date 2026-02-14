f3a8f9af690620e3438b4146e8758cab5cd0a125
yizbtc7 <261330030+yizbtc7@users.noreply.github.com>
2026-02-13 21:33:55 -0500

    personalization: support spaced/case-insensitive {child_name} placeholders

--- next-app/src/utils/personalize.ts
+++ next-app/src/utils/personalize.ts
@@ -1,5 +1,5 @@
 export const replaceChildName = (text: string, name: string) =>
-  text.replace(/\{\{child_name\}\}|\{child_name\}/g, name);
+  text.replace(/\{\{\s*child_name\s*\}\}|\{\s*child_name\s*\}/gi, name);
 
 export const replaceChildNameList = (items: string[], name: string) =>
   items.map((item) => replaceChildName(item, name));
